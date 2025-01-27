d5e0a33fbfa070de60e91b99159e37f21e5b5604
Alejandro Bautista <alejandro.hernandez4819@alumnos.udg.mx>
2025-01-27 12:57:24 -0600

    Add: Verificacion de numero secreto

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
-var titulo = document.querySelector('h1');
-titulo.innerHTML = 'Juego del número secreto';
+var numeroSecreto = GenerarNumeroSecreto();
 
-var parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Adivina el número secreto que está entre 1 y 100';
+
+function verificarIntento() {
+    var numeroIngresado = parseInt(document.getElementById('valorUsuario').value);
+
+    console.log(numeroSecreto);
+    if(numeroIngresado === numeroSecreto){
+        asignarTextoElemento('h1', '¡Felicitaciones! Ganaste');
+        asignarTextoElemento('p', 'El número secreto era ' + numeroSecreto);
+    }
+    else{
+        if(numeroIngresado > numeroSecreto){
+            asignarTextoElemento('h1', '¡Fallaste! El número secreto es menor');
+        }
+        else{
+            asignarTextoElemento('h1', '¡Fallaste! El número secreto es mayor');
+        }
+    }
+    return;
+}
+
+function asignarTextoElemento(elemento, texto) {
+    var elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+    
+}
+
+function GenerarNumeroSecreto() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+asignarTextoElemento('h1', 'Juego del número secreto');
+asignarTextoElemento('p', 'Adivina el número secreto que está entre 1 y 10');
