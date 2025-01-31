8887a9e4dfc363f9a76a7ead4d228d9837fbb3c4
Alejandro Bautista <alejandro.hernandez4819@alumnos.udg.mx>
2025-01-31 12:36:57 -0600

    Add: Funciones

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var numeroSecreto = GenerarNumeroSecreto();
-var intetos = 1;
+var numeroSecreto = 0;
+var intetos = 0;
 
 
 function verificarIntento() {
@@ -8,9 +8,10 @@ function verificarIntento() {
     console.log(numeroSecreto);
     if(numeroIngresado === numeroSecreto){
         asignarTextoElemento('p', `¡Felicitaciones! Ganaste en ${intetos} ${intetos === 1 ? 'intento' : 'intentos'}`);	
-        
+        document.getElementById('reiniciar').removeAttribute('disabled');
     }
     else{
+        // El usuario no acertó.
         if(numeroIngresado > numeroSecreto){
             asignarTextoElemento('p', '¡Fallaste! El número secreto es MENOR');
         }
@@ -18,8 +19,13 @@ function verificarIntento() {
             asignarTextoElemento('p', '¡Fallaste! El número secreto es MAYOR');
         }
         intetos++;
+        limpiarCaja();
     }
     return;
+}
+
+function limpiarCaja(){
+    document.querySelector('#valorUsuario').value = '';
 }
 
 function asignarTextoElemento(elemento, texto) {
@@ -33,5 +39,22 @@ function GenerarNumeroSecreto() {
     return Math.floor(Math.random() * 10) + 1;
 }
 
-asignarTextoElemento('h1', 'Juego del número secreto');
-asignarTextoElemento('p', 'Adivina el número secreto que está entre 1 y 10');
+function condicionesIniciales(){
+    asignarTextoElemento('h1', 'Juego del número secreto');
+    asignarTextoElemento('p', 'Adivina el número secreto que está entre 1 y 10');
+    //Generar el numero secreto
+    numeroSecreto = GenerarNumeroSecreto();
+    //Inicializar el contador de intentos
+    intetos = 1;
+}
+function reiniciarJuego(){
+    //Limpiar la caja
+    limpiarCaja();
+    //Indicar mensaje de intervalo de numeros
+    condicionesIniciales();
+    //Deshabilitat el boton de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled', 'true');  
+    
+}
+
+condicionesIniciales();
